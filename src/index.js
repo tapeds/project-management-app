db908a6c2f41fef253ee468003476b13f82471cc
tapeds <frrllmatthew@gmail.com>
2024-02-22 23:57:13 +0700

    feat: add new font

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ import { Toaster } from "react-hot-toast";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
+        rel="stylesheet"
+      />
+    </head>
     <Toaster />
     <App />
   </React.StrictMode>
