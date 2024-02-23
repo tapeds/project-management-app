37eb59be27e26a07a6f0cd16bb6624bf91b5be63
tapeds <frrllmatthew@gmail.com>
2024-02-23 16:33:57 +0700

    feat: add edit state

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { FiTrash2 } from "react-icons/fi";
+import { FaEdit } from "react-icons/fa";
 
 function Card({ title, description, index, data, storage }) {
+  const [edit, setEdit] = useState(false);
+
   const onClick = () => {
     const newData = data.filter((_, i) => i !== index);
     localStorage.setItem(`${storage}`, JSON.stringify(newData));
@@ -31,7 +35,10 @@ function Card({ title, description, index, data, storage }) {
   };
 
   return (
-    <div className="w-full bg-[#F6F6F6] py-2 flex flex-col items-center justify-center px-4 rounded-md group transition-all duration-300">
+    <div
+      className="w-full bg-[#F6F6F6] py-2 flex flex-col items-center justify-center px-4 rounded-md group transition-all duration-300"
+      onMouseLeave={() => setEdit(false)}
+    >
       <div className="flex justify-between items-center">
         <div className="flex flex-col">
           <h5 className="text-base font-semibold text-gray-900">{title}</h5>
@@ -39,24 +46,33 @@ function Card({ title, description, index, data, storage }) {
             {description}
           </p>
         </div>
-        <FiTrash2
-          className="size-4 hover:cursor-pointer group-hover:opacity-100 text-red-500 opacity-0"
-          onClick={() => onClick()}
-        />
+        {!edit ? (
+          <FaEdit
+            className="size-4 hover:cursor-pointer group-hover:opacity-100 text-gray-900 opacity-0"
+            onClick={() => setEdit(!edit)}
+          />
+        ) : (
+          <FiTrash2
+            className="size-4 hover:cursor-pointer group-hover:opacity-100 text-red-500 opacity-0"
+            onClick={() => onClick()}
+          />
+        )}
       </div>
-      <div className="group-hover:block hidden mt-1.5">
-        <select
-          className="py-1 px-2 rounded-lg border"
-          onChange={handleSelectChange}
-          defaultValue={"Move to"}
-        >
-          <option disabled>Move to</option>
-          {storage !== "To Do" && <option>To Do</option>}
-          {storage !== "In Progress" && <option>In Progress</option>}
-          {storage !== "In Revision" && <option>In Revision</option>}
-          {storage !== "Done" && <option>Done</option>}
-        </select>
-      </div>
+      {edit && (
+        <div className="group-hover:block hidden mt-1.5">
+          <select
+            className="py-1 px-2 rounded-lg border"
+            onChange={handleSelectChange}
+            defaultValue={"Move to"}
+          >
+            <option disabled>Move to</option>
+            {storage !== "To Do" && <option>To Do</option>}
+            {storage !== "In Progress" && <option>In Progress</option>}
+            {storage !== "In Revision" && <option>In Revision</option>}
+            {storage !== "Done" && <option>Done</option>}
+          </select>
+        </div>
+      )}
     </div>
   );
 }
